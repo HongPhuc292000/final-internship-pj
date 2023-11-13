72b0434544393259e01058848fcad083409df3fc
Helen <hongphuc292000@gmail.com>
2023-11-13 09:51:12 +0700

    initial project

---
+++ src/filters/any-exception.filter.ts
@@ -0,0 +1,34 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  HttpExceptionBody,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class AnyExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let error = 'Internal Server Error';
+    let message: string | string[] = 'unknown error';
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const exceptionBody = exception.getResponse() as HttpExceptionBody;
+      error = exceptionBody.error;
+      message = exception.message;
+    }
+    response.status(status).json({
+      message,
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error: error,
+    });
+  }
+}
